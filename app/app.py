15b9fd0128fdf4cf62c4b0da8aa7a05f921e6606
Marc-Antoine Ferland <maf_ferland@hotmail.com>
2016-01-26 13:24:35 -0500

    Added text input, save input in file on server, return input in the web page

--- app/app.py
+++ app/app.py
@@ -1,11 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        code = request.form["text"]
+        with open('Failed.py', 'w') as file_:
+            file_.write(code)
+        return render_template('index.html', code=code)
+    elif request.method == 'GET':
+        return render_template('index.html', code="")
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
